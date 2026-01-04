17bba0845b0b1171a536070cb4750358c11f1fcd
hauses <80683694+JordiCasasPla@users.noreply.github.com>
2026-01-04 19:37:14 +0100

    ccc

---
+++ packages/react-sdk/vite.config.ts
@@ -0,0 +1,32 @@
+import { resolve } from "path";
+import preserveDirectives from "rollup-preserve-directives";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      exclude: ["**/*.test.ts", "**/*.test.tsx"]
+    }),
+    preserveDirectives(),
+  ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "HausesFlagsReactSDK",
+      fileName: "index",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "@hauses/flags-core"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
